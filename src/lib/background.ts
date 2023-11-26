445fadb4d04ba705db559fbd76ca83c86d2ccefd
Chang Xiao <chang.x1994@gmail.com>
2023-11-26 01:02:43 -0500

    Added shadows to background

--- src/lib/background.ts
+++ src/lib/background.ts
@@ -17,13 +17,11 @@ const camera = new THREE.PerspectiveCamera(30, win.w/win.h, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 
 // THREE helper variables
-const objects: Array<THREE.Mesh> = [];
 const vec = new THREE.Vector3();
 const pos = new THREE.Vector3();
 const darkColor = new THREE.Color(0x2a2a33);
 const lightColor = new THREE.Color(0xb6b6d6);
 const useLightTheme = (): boolean => document?.body?.dataset?.theme === "light";
-let count = 0;
 
 export function init(ref: HTMLDivElement) {
   if (!document || !ref) return;
@@ -38,41 +36,36 @@ export function init(ref: HTMLDivElement) {
   renderer.shadowMap.enabled = true;
   ref.appendChild( renderer.domElement );
 
-  const ambient = new THREE.AmbientLight(0x333333, 0.5);
-  const light = new THREE.PointLight(0xd0d0ff, 1, 1000, 0.1);
-  light.position.set(0, 0, 100);
-  scene.add(ambient, light);
+  const ambient = new THREE.AmbientLight(0x4f4f4f, 1);
+  const light = new THREE.PointLight(0xd0d0ff, 1, 100, 0.5);
+  light.castShadow = true;
+  light.position.set(0, 10, 25);
 
   // add objects
-  const cube = addCube();
-  const plane = addPlane();
-  objects.push(cube, plane);
+  const cube = createCube();
+  const plane = createPlane();
+  scene.add(ambient, light, cube, plane);
   animate();
+  console.log(scene.children);
 }
 
-function addCube(): THREE.Mesh {
-  const geometry = new THREE.BoxGeometry(4, 4, 4);
-  const material = new THREE.MeshStandardMaterial({ color:0xddeeff });
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add(cube);
-  return cube;
-}
-
-function addPlane(): THREE.Mesh {
-  const geometry = new THREE.PlaneGeometry(200, 200, 40, 40);
-  const material = new THREE.MeshStandardMaterial({ color:0xffffff, wireframe:true });
-  const plane = new THREE.Mesh(geometry, material);
-  // plane.position.z = -10;
-  scene.add(plane);
-  return plane;
-}
-
-async function animate() {
+function animate(): void {
   requestAnimationFrame(animate);
 
   // toggle bg color based on theme
-  if (useLightTheme()) scene.background = lightColor;
-  else scene.background = darkColor;
+  const pLight = scene.children[1] as THREE.PointLight;
+  const cube = scene.children[2] as THREE.Mesh;
+  const plane = scene.children[3] as THREE.Mesh;
+  const planeMat = plane.material as THREE.MeshStandardMaterial;
+  if (useLightTheme()) {
+    scene.background = lightColor;
+    pLight.decay = 0.1;
+    planeMat.color.set(0xffffff);
+  } else {
+    scene.background = darkColor;
+    pLight.decay = 0.5;
+    planeMat.color.set(0xdfdfdf);
+  }
 
   // keep canvas size updated with window size
   camera.aspect = win.w/win.h;
@@ -80,35 +73,40 @@ async function animate() {
   renderer.setSize(win.w, win.h);
   calcMousePos();
 
-  // handle object 0 (cube)
-  objects[0].rotation.x += 0.01;
-  objects[0].rotation.y += 0.01;
-  objects[0].position.set(pos.x, pos.y, pos.z);
+  // handle cube
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  cube.position.set(pos.x, pos.y, pos.z);
   clearVecs();
-
-  // handle object 1 (plane)
-  const position = objects[1].geometry.attributes.position;
-  for (let i=0; i < position.count; i++) {
-    vec.fromBufferAttribute(position, i);
-    position.setXYZ(
-      i, 
-      vec.x, 
-      vec.y, 
-      -10 + 2 * Math.sin(i + count * 0.0001)
-    );
-    position.needsUpdate = true;
-    count += 0.1;
-  }
-  if (count > 1000000) {
-    console.log("reset loop", count);
-    count = 0;
-  }
 
   // render new frame
   renderer.render( scene, camera );
 }
 
-function calcMousePos(zPos:number = 0) {
+// -- HELPER FUNCTIONS --
+function createCube(): THREE.Mesh {
+  const geometry = new THREE.BoxGeometry(4, 4, 4);
+  const material = new THREE.MeshStandardMaterial({ color:0xabcaff });
+  const cube = new THREE.Mesh( geometry, material );
+  cube.castShadow = true;
+  return cube;
+}
+
+function createPlane(): THREE.Mesh {
+  const geometry = new THREE.PlaneGeometry(200, 200, 40, 40);
+  const material = new THREE.MeshStandardMaterial({ color:0xdfdfdf });
+  // const material = new THREE.ShaderMaterial({
+  //   uniforms: {},
+  //   vertexShader: planeVert,
+	//   fragmentShader: planeFrag
+  // });
+  const plane = new THREE.Mesh(geometry, material);
+  plane.receiveShadow = true;
+  plane.position.z = -10;
+  return plane;
+}
+
+function calcMousePos(zPos:number = 0): void {
   if (win.x && win.y) vec.set(2*(win.x/win.w) - 1, 1 - 2*(win.y/win.h), 0.5);
   else vec.set(0, 0, 0);
   vec.unproject(camera);
@@ -117,7 +115,7 @@ function calcMousePos(zPos:number = 0) {
   pos.copy(camera.position).add(vec.multiplyScalar(distance));
 }
 
-function clearVecs() {
+function clearVecs(): void {
   vec.set(0,0,0);
   pos.set(0,0,0);
 }
